f8e346b98fd1ea7f0ed71ed96fa721a25c93a267
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-04-07 22:36:07 +0000

    add basic color theme and font

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module JamaicaPicGallery
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.assets.image_optim = false
   end
 end
